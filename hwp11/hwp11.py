9aac43a5cc257d01212dc9dd2160fd1fc885be2c
nikitandronov <nikitandronov@gmail.com>
2017-02-13 21:26:13 +0300

    Add files via upload

---
+++ hwp11/hwp11.py
@@ -0,0 +1,13 @@
+import re
+
+
+with open('викинги.txt', 'r', encoding='utf-8') as f:
+    t = f.read()
+
+m = re.sub(u'викинг', u'бурундук',t)
+m = re.sub(u'Викинг', u'Бурундук',m)
+
+with open('vysupir.txt', 'w', encoding='utf-8') as d:
+    d.write(m)
+
+print('Вы молодец - см.файд vysupir')
