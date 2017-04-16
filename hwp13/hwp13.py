7e1451dc88324186238c3a112e6963ca06e946df
nikitandronov <nikitandronov@gmail.com>
2017-04-17 00:28:31 +0300

    Update hwp13.py

--- hwp13/hwp13.py
+++ hwp13/hwp13.py
@@ -1,13 +1,9 @@
+import os
 
+folder = 'C:\\hwp\\'
+print(os.listdir(folder))
 
-
-with open('викинги.txt', 'r', encoding='utf-8') as f:
-    t = f.read()
-
-m = re.sub(u'викинг', u'бурундук',t)
-m = re.sub(u'Викинг', u'Бурундук',m)
-
-with open('vysupir.txt', 'w', encoding='utf-8') as d:
-    d.write(m)
-
-print('Вы молодец - см.файд vysupir')
+for f in os.listdir(folder):
+     with open(os.path.join(folder, f)) as text:
+        if f.isalpha():
+            print(f)
